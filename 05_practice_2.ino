db3cf47c15b04a5d044ec0579e4e72a52b3ee85b
ChaeJoowon <jjww0604@gmail.com>
2020-09-18 12:54:34 +0900

    Update 05_practice_2.ino

--- 05_practice_2.ino
+++ 05_practice_2.ino
@@ -7,9 +7,9 @@ void loop() {
   digitalWrite(PIN_LED, 0);
   delay(1000);
   for(int i=0; i<5; i++){
-  digitalWrite(PIN_LED, 0);
-  delay(100);
   digitalWrite(PIN_LED, 1);
+  delay(100);
+  digitalWrite(PIN_LED, 0);
   delay(100);
   }
   while(1){
